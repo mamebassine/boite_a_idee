b3fe19005d3518719eb1d3abc75728db643634ea
mamebassine <mamebassine06@gmail.com>
2024-03-10 00:46:56 +0000

    Quelques modification ma page de connection a la basee connectionBD.php

--- connectionBD.php
+++ connectionBD.php
@@ -1,15 +1,17 @@
 <?php
-// Informations de connexion à la base de données
-$serveur = "localhost";             // Nom du serveur MySQL
-$utilisateur = "root";              // Nom d'utilisateur MySQL
-$mot_de_passe = "";                  // Mot de passe MySQL 
-$base_de_donnees = "application_boite_a_idee_collaborative"; // Nom de la base de données MySQL
-// Initialisation de la connexion en dehors de la condition
-$connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
-// Vérification de la réussite de la connexion
-if ($connexion->connect_error) {
-    // Si la connexion échoue, affiche un message d'erreur et arrête le script
-    die("La connexion a échoué : " . $connexion->connect_error);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "application_boite_a_idee_collaborative";
+
+function connect()
+{
+    $conn = new mysqli($GLOBALS['servername'], $GLOBALS['username'], $GLOBALS['password'], $GLOBALS['dbname']);
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("La connexion a échoué : " . $conn->connect_error);
+    }
+return $conn;
 }
-// À ce stade, la connexion à la base de données a réussi et est stockée dans la variable $connexion. 
 ?>
